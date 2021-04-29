5c11dcf8405dcd5498e4e09b0378223da1e44780
M.J. Heuveling <misja@omineus.mgmt>
2021-04-29 19:50:35 +0200

    changed substitution pattern fix

--- Products/substitute.sh
+++ Products/substitute.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 file=$1
 echo "replacing substitutions with environment vars for file $file"
-list=$(grep -wo "\\$\S\S*\\}" Dockerfile| sort -u | tr -d '${}')
+list=$(grep -wo "\\$\S\S*\\}" $file| sort -u | tr -d '${}')
 echo $list;
-for thing in ${list[@]}; do sed -i 's/\${'$thing'}/'$"${!thing}"'/g' $file; done
+for thing in ${list[@]}; do  sed -i 's/\${'$thing'}/'$"${!thing}"'/g' $file; done
