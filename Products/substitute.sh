e06c85bc92fcc0732ad338c5d32a367cb07fd43a
M.J. Heuveling <misja@omineus.mgmt>
2021-04-29 20:28:20 +0200

    move substitutions into jenkinsfile

--- Products/substitute.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-file=$1
-echo "replacing substitutions with environment vars for file $file"
-list=$(grep -wo "\\$\S\S*\\}" $file| sort -u | tr -d '${}')
-echo $list;
-for thing in ${list[@]}; do  sed -i 's/\"${"'$thing'"}"/'$"${!thing}"'/g' $file; done
