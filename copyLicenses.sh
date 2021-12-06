6270b913eeaa6141643970f7ea1771aa33f3c63c
Misja Heuveling <misja.heuveling@softwareag.com>
2021-12-06 12:33:43 +0100

    fixed issue with spaces in file name

--- copyLicenses.sh
+++ copyLicenses.sh
@@ -5,13 +5,14 @@ if [ -f licenses.zip ]; then
   cd licenses
   unzip ../licenses.zip
   IS_LICENSE=$(find . -name \*Integration\?Server\*.xml)
+  mv "$IS_LICENSE" ISLicense.xml
+  cd ..
   kubectl apply -f licenseUploadPod.yml
   POD=$(kubectl -n cicd get pods -l pod=swag-licenses --output=jsonpath={.items..metadata.name})
-  kubectl -n cicd wait --for=condition=Ready pod/$POD 
-  kubectl -n cicd cp $IS_LICENSE $POD:/licenses/ISLicense.xml
+  kubectl -n cicd wait --for=condition=Ready pod/$POD
+  kubectl -n cicd cp ./licenses/ISLicense.xml $POD:/licenses/ISLicense.xml
   kubectl -n cicd cp SoftwareAGInstaller*.bin $POD:/licenses/SoftwareAGInstaller.bin
   kubectl -n cicd delete pod $POD
-  cd ..
   rm -rf licenses
 else
   echo "No license zip file found. Please place you zip file containing your licenses in this directory as licenses.zip"
