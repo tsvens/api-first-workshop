01b3c46fff42a705d161a064653ed63ef024099d
Misja Heuveling <misja.heuveling@softwareag.com>
2022-12-12 10:44:29 +0100

    updated license upload POD to be a DaemonSet to ensure licenses are copied to all nodes

--- copyLicenses.sh
+++ copyLicenses.sh
@@ -10,13 +10,16 @@ if [ -f licenses.zip ]; then
   mv "$WMT_LICENSE" WmTestSuite100.xml
   cd ..
   kubectl apply -f licenseUploadPod.yml
-  POD=$(kubectl -n cicd get pods -l pod=swag-licenses --output=jsonpath={.items..metadata.name})
-  kubectl -n cicd wait --for=condition=Ready pod/$POD
-  kubectl -n api-first-workshop create configmap msr-license --from-file=./licenses/ISLicense.xml
-  kubectl -n cicd cp ./licenses/ISLicense.xml $POD:/licenses/ISLicense.xml
-  kubectl -n cicd cp ./licenses/WmTestSuite100.xml $POD:/licenses/WmTestSuite100.xml
-  kubectl -n cicd cp SoftwareAGInstaller*.bin $POD:/licenses/SoftwareAGInstaller.bin
-  kubectl -n cicd delete pod $POD
+  PODs=$(kubectl -n cicd get pods -l pod=swag-licenses --output=jsonpath={.items..metadata.name})
+  for POD in $PODs
+  do
+    kubectl -n cicd wait --for=condition=Ready pod/$POD
+    kubectl -n api-first-workshop create configmap msr-license --from-file=./licenses/ISLicense.xml
+    kubectl -n cicd cp ./licenses/ISLicense.xml $POD:/licenses/ISLicense.xml
+    kubectl -n cicd cp ./licenses/WmTestSuite100.xml $POD:/licenses/WmTestSuite100.xml
+    kubectl -n cicd cp SoftwareAGInstaller*.bin $POD:/licenses/SoftwareAGInstaller.bin
+  done
+  kubectl -n cicd delete daemonset swag-licenses
   rm -rf licenses
 else
   echo "No license zip file found. Please place you zip file containing your licenses in this directory as licenses.zip"
